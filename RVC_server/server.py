66cb0280e39a9e1db8d75dd68cca2c0ae2253471
Yoojin <jyoovision@gmail.com>
2023-11-20 03:38:26 +0900

    update RVC_server for GCS

--- RVC_server/server.py
+++ RVC_server/server.py
@@ -1,10 +1,31 @@
-import websockets
-import asyncio
+import os
 import struct
+import asyncio
+import websockets
 from io import BytesIO
-from pydub import AudioSegment, silence
-from concurrent.futures import ProcessPoolExecutor
 from simpleRVC import RVC
+from dotenv import load_dotenv
+from configs.config import Config
+from pydub import AudioSegment, silence
+from infer.modules.vc.modules import VC
+from concurrent.futures import ThreadPoolExecutor
+from google.cloud import storage
+
+# from google.cloud import speech
+# from google.cloud.speech import enums
+# from google.cloud.speech import types
+# from google.oauth2 import service_account
+
+# Init/초기 설정
+load_dotenv()
+vc = VC(Config())
+sid_value = "yoojin.pth"
+protect_value = 0.33
+vc.get_vc(sid_value, protect_value, protect_value)
+
+google_path = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
+storage_client = storage.Client()
+bucket = storage_client.bucket("audio_segments")
 
 
 # 언리얼 클라이언트와 웹소켓 서버 통신
@@ -16,19 +37,18 @@ async def echo(websocket, path):
         # start RVC 메시지가 들어오면 is_rvc_running을 검사하여 RVC 수행
         if message == "start RVC":
             send_start_task = asyncio.create_task(websocket.send("RVC start"))
-            rvc_task = asyncio.create_task(RVC())
+            rvc_task = asyncio.create_task(RVC(vc))
             _, audio = await asyncio.gather(send_start_task, rvc_task)
 
             send_complete_task = asyncio.create_task(websocket.send("RVC complete"))
             extract_send_last_word_task = asyncio.create_task(
                 extract_send_last_word(websocket, audio)
             )
-            # splite_save_to_DB_task = asyncio.create_task(
-            #     splite_save_to_DB(websocket, audio)
-            # )
+            splite_save_to_DB_task = asyncio.create_task(
+                splite_save_to_DB(websocket, audio)
+            )
             await asyncio.gather(
-                send_complete_task,
-                extract_send_last_word_task,
+                send_complete_task, extract_send_last_word_task, splite_save_to_DB_task
             )
 
         # 다른 메시지가 들어오면 처리
@@ -71,20 +91,46 @@ async def extract_send_last_word(websocket, audio):
     await websocket.send("Successfully sending binary data")
 
 
+# 세그먼트를 구글 클라우드 스토리지에 저장하는 함수
+def save_segment_to_gcs(segment, segment_number):
+    # 메모리 상의 BytesIO 스트림에 세그먼트 저장
+    buffer = BytesIO()
+    segment.export(buffer, format="wav")
+    buffer.seek(0)  # 스트림을 처음으로 되돌림
+
+    # 파일명 생성 (예: segment_1.wav)
+    segment_name = f"segment_{segment_number}.wav"
+
+    # 구글 클라우드 스토리지에 업로드
+    blob = bucket.blob(segment_name)
+    blob.upload_from_file(buffer, content_type="audio/wav")
+
+    # 필요한 경우, 데이터베이스에 세그먼트 정보 저장
+    # 예: database.save(segment_info)
+
+    return f"{segment_name} uploaded to GCS"
+
+
+# 오디오 세그먼트를 병렬로 저장하는 함수
 async def splite_save_to_DB(websocket, audio):
-    # 오디오를 세그먼트로 나눠 데이터 베이스에 저장하는 로직을 추가할 예정
     segments = silence.split_on_silence(
         audio, min_silence_len=200, silence_thresh=audio.dBFS - 25
     )
 
-    with ProcessPoolExecutor() as executor:
-        results = list(executor.map(save_segment_to_db, segments))
+    loop = asyncio.get_running_loop()
 
-    return None
+    # ThreadPoolExecutor를 사용하여 세그먼트 저장 작업을 병렬로 수행
+    with ThreadPoolExecutor() as executor:
+        tasks = [
+            loop.run_in_executor(executor, save_segment_to_gcs, segment, i)
+            for i, segment in enumerate(segments)
+        ]
 
+        # 모든 작업이 완료될 때까지 기다림
+        await asyncio.gather(*tasks)
 
-async def save_segment_to_db(segment):
-    return "Segment saved successfully"
+    print("GCS save complete")
+    await websocket.send("GCS save complete")
 
 
 def verify_wav_format(audio_binary):
